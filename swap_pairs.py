a2acca915dbe6c53d6553e0855c76bcaebb5dc07
Nikx999 <43091564+Nikx999@users.noreply.github.com>
2023-10-10 19:32:32 +0530

    Update swap_pairs.py

--- swap_pairs.py
+++ swap_pairs.py
@@ -1,3 +1,8 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
@@ -5,24 +10,22 @@ class Solution:
         new_head = None
         flag = 0
         prev = None
-        while(head and head.next):
-            print(head.val)
-            
+
+        while(head and head.next):            
             temp = head
             head = head.next
             temp.next = head.next
             head.next = temp
             if flag != 0:
                 prev.next = head
-            print(temp.val,head.val)
             if flag==0: 
                 new_head = head
                 flag+=1
             prev = head.next
-            head = head.next.next
-            
+            head = head.next.next 
             
         if flag==0:
             return head
         else:
             return new_head
+
